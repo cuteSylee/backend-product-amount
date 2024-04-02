362ad3964f06223d02f45d7ee2795c102eca49c2
cuteSylee <soyun7707@naver.com>
2024-04-03 07:03:39 +0900

    Revert "Revert "Revert "Revert "entity,repository JPA 수정""""
    This reverts commit 978a2148821f3f7123449b5660255096f177efac.

---
+++ src/main/java/antigravity/repository/PromotionRepository.java
@@ -0,0 +1,8 @@
+package antigravity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import antigravity.domain.entity.Promotion;
+
+public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+
+}
--- src/main/java/antigravity/service/ProductService.java
+++ src/main/java/antigravity/service/ProductService.java
@@ -1,22 +1,43 @@
 package antigravity.service;
 
 import antigravity.domain.entity.Product;
+import antigravity.domain.entity.PromotionProducts;
 import antigravity.model.request.ProductInfoRequest;
 import antigravity.model.response.ProductAmountResponse;
 import antigravity.repository.ProductRepository;
+import antigravity.repository.PromotionProductsRepository;
+import antigravity.repository.PromotionRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class ProductService {
-    private final ProductRepository repository;
+	private final ProductRepository productRepository;
+	private final PromotionRepository promotionRepository;
 
     public ProductAmountResponse getProductAmount(ProductInfoRequest request) {
-        System.out.println("상품 가격 추출 로직을 완성 시켜주세요.");
+    
 
-        Product product = repository.getProduct(request.getProductId());
+        Product product = productRepository.findById(request.getProductId()).orElseThrow();
+        
+        for(long couponId : request.getCouponIds()) {
+        	System.out.println(couponId);
+        }
 
-        return null;
+        System.out.println("ggggg");
+
+        return ProductAmountResponse.builder()
+        		.name("")
+        		.originPrice(110)
+        		.discountPrice(0)
+        		.finalPrice(0)
+        		.build();
     }
+    
+    
 }
